6d4f2bddf7c5b873680124bd1be0e8b5b44bf04d
OneProg <jade.weirdo@gmail.com>
2025-12-15 18:11:37 +0300

    Version display

--- src/navbar.rs
+++ src/navbar.rs
@@ -4,59 +4,36 @@ use crate::route::Route;
 
 #[component]
 pub fn Navbar() -> Element {
+    const VERSION: &str = env!("CARGO_PKG_VERSION");
+
     rsx! {
-        div { class: "navbar bg-base-200 flex flex-col gap-5 items-start justify-start",
+        div {
+            class: "navbar bg-base-200 flex flex-col gap-5 items-start justify-start",
             id: "navbar",
 
-            h1 { class: "text-4xl font-bold",
-                "OneProg Контест"
-            }
+            h1 { class: "text-4xl font-bold", {format!("OneProg Контест v{VERSION}")} }
 
             div { role: "tablist", class: "tabs tabs-box gap-2 bg-base-300",
-                Link { role: "tab", class: "tab",
-                    to: Route::Home {},
-                    "Главная"
-                }
+                Link { role: "tab", class: "tab", to: Route::Home {}, "Главная" }
 
-                Link { role: "tab", class: "tab",
-                    to: Route::Problems {},
-                    "Задачи"
-                }
+                Link { role: "tab", class: "tab", to: Route::Problems {}, "Задачи" }
 
-                Link { role: "tab", class: "tab",
-                    to: Route::Contests {},
-                    "Контесты"
-                }
+                Link { role: "tab", class: "tab", to: Route::Contests {}, "Контесты" }
 
-                Link { role: "tab", class: "tab",
-                    to: Route::Competitions {},
+                Link { role: "tab", class: "tab", to: Route::Competitions {},
                     "Соревнования"
                 }
 
-                Link { role: "tab", class: "tab",
-                    to: Route::Market {},
-                    "Рынок"
-                }
+                Link { role: "tab", class: "tab", to: Route::Market {}, "Рынок" }
 
-                Link { role: "tab", class: "tab",
-                    to: Route::Business {},
-                    "Бизнес"
-                }
+                Link { role: "tab", class: "tab", to: Route::Business {}, "Бизнес" }
 
-                Link { role: "tab", class: "tab",
-                    to: Route::Support {},
-                    "Поддержать"
-                },
+                Link { role: "tab", class: "tab", to: Route::Support {}, "Поддержать" }
 
-                Link { role: "tab", class: "tab",
-                    to: Route::Account {},
-                    "Аккаунт"
-                }
+                Link { role: "tab", class: "tab", to: Route::Account {}, "Аккаунт" }
             }
         }
 
-        div { class: "m-2",
-            Outlet::<Route> { }
-        }
+        div { class: "m-2", Outlet::<Route> {} }
     }
 }
