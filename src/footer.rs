3cde76b1f69442739651db6de5025d42ba9a72ab
OneProg <jade.weirdo@gmail.com>
2026-01-18 19:54:26 +0300

    Delete useless styles

--- src/footer.rs
+++ src/footer.rs
@@ -3,10 +3,7 @@ use dioxus::prelude::*;
 #[component]
 pub fn Footer() -> Element {
     rsx! {
-        div {
-            class: "footer sm:footer-horizontal footer-center bg-base-200 text-base-content p-4",
-            id: "footer",
-
+        div { class: "footer footer-center bg-base-200 p-4", id: "footer",
             p { "© 2026 OneProg" }
         }
     }
