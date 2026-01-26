f6c9715a6c25dd6cc5111e9ab7d66c97e4d1a8a9
OneProg <jade.weirdo@gmail.com>
2026-01-26 21:54:26 +0300

    Change to 404 page. Some stuff in account page. Update to rust 1.93 & crates updates

--- src/tabs/account.rs
+++ src/tabs/account.rs
@@ -1,8 +1,29 @@
 use dioxus::prelude::*;
 
+use crate::app_state::ClientAppState;
+
 #[component]
 pub fn Account() -> Element {
+    let state = use_context::<ClientAppState>();
+    let user = state.user.read();
+
     rsx! {
+        div { class: "flex flex-col gap-2",
+            if let Some(user) = user.as_ref() {
+                h1 { class: "text-sm italic", "{user.username}" }
+                h1 {
+                    if user.admin {
+                        "Администратор"
+                    } else {
+                        "Не администратор"
+                    }
+                }
+                button { class: "btn btn- w-auto", "Выйти" }
+            } else {
+                button { class: "btn btn-primary w-auto", "Зарегистрироваться" }
+                button { class: "btn btn-secondary w-auto", "Войти" }
+            }
+        }
         div { class: "divider" }
     }
 }
